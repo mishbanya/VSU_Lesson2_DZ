da937d45411cf70f38dd0c341ae1c591eec2784a
Degtyarev111 <degtyarevmishas@yandex.ru>
2024-04-07 16:03:38 +0300

    smth

--- app/src/main/java/com/example/vsu_lesson2_dz/MainActivity.kt
+++ app/src/main/java/com/example/vsu_lesson2_dz/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.vsu_lesson2_dz
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private var secretKey: String? = null
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,46 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val contentProviderButton: Button = findViewById(R.id.contentprovider_button)
+        val broadcastReceiverButton: Button = findViewById(R.id.broadcastreciever_button)
+        contentProviderButton.setOnClickListener{
+            val uri = Uri.parse("content://dev.surf.android.provider/text")
+
+            val cursor = contentResolver.query(uri, null, null, null, null)
+            cursor?.use {
+                if (it.moveToFirst()) {
+                    val text = it.getString(it.getColumnIndex("text"))
+                    makeAToastMsg(text)
+                    secretKey = text
+                } else {
+                    makeAToastMsg("nokey")
+                }
+            }
+        }
+        broadcastReceiverButton.setOnClickListener {
+            makeAToastMsg("b2")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("CONTENT_PROVIDER_KEY", secretKey)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val contentProviderKey = savedInstanceState?.getString("CONTENT_PROVIDER_KEY")
+        if (contentProviderKey != null) {
+            Log.d("CONTENT_PROVIDER_KEY", "$contentProviderKey")
+            secretKey = contentProviderKey
+        }
+    }
+    fun makeAToastMsg(s: String){
+        Toast.makeText(
+            this,
+            s,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
